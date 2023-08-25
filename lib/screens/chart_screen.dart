b7fdd8c9c420cb87c81deec8b471942340f1872f
DmitrySergienko <dmitrisergienko@gmail.com>
2023-08-25 15:41:56 +0300

    SignOut Firebase implemented

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,13 +1,25 @@
+import 'package:chat_app/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
+
+  void _logOut() {
+    firebase.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter chart'),
+        actions: [
+          IconButton(
+            onPressed: _logOut,
+            icon: const Icon(Icons.exit_to_app),
+            color: Theme.of(context).colorScheme.primary,
+          )
+        ],
       ),
       body: const Center(
         child: Text('Logged In'),
